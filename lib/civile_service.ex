556dd749bd712448b251cdaf5ee1e09cd8140d92
Carlo Gilmar <carlogilmar12@gmail.com>
2020-02-10 16:14:43 -0600

    10) Create the API on civile service for make Riak operations

--- lib/civile_service.ex
+++ lib/civile_service.ex
@@ -1,12 +1,24 @@
 defmodule Civile.Service do
 
-  def ping(v\\1) do
-    idx = :riak_core_util.chash_key({"civile", "ping#{v}"})
+  def ping(v \\ 1) do
+    send_cmd("ping#{v}", {:ping, v})
+  end
+
+  def put(k, v) do
+    send_cmd(k, {:put, {k, v}})
+  end
+
+  def get(k) do
+    send_cmd(k, {:get, k})
+  end
+
+  defp send_cmd(k, cmd) do
+    idx = :riak_core_util.chash_key({"civile", k})
     pref_list = :riak_core_apl.get_primary_apl(idx, 1, Civile.Service)
 
     [{index_node, _type}] = pref_list
 
-    :riak_core_vnode_master.sync_command(index_node, {:ping, v}, Civile.VNode_master)
+    :riak_core_vnode_master.sync_command(index_node, cmd, Civile.VNode_master)
   end
 
 end
