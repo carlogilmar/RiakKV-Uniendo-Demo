d761776078d24eaff849745ac7c8e1388fb1c165
Carlo Gilmar <carlogilmar12@gmail.com>
2020-02-10 15:26:06 -0600

    7) Modify application and supervisor

--- lib/civile.ex
+++ lib/civile.ex
@@ -1,13 +1,15 @@
 defmodule Civile do
-  use Application
-  require Logger
+	use Application
+	require Logger
 
-  def start(_type, _args) do
-    case Civile.Supervisor.start_link do
-      {:ok, pid} ->
-        {:ok, pid}
-      {:error, reason} ->
-        Logger.error("Unable to start Civile supervisor because: #{inspect reason}")
-    end
-  end
+	def start(_type, _args) do
+		case Civile.Supervisor.start_link do
+			{:ok, pid} ->
+				:ok = :riak_core.register(vnode_module: Civile.VNode)
+				:ok = :riak_core_node_watcher.service_up(Civile.Service, self())
+				{:ok, pid}
+			{:error, reason} ->
+				Logger.error("Unable to start Civile supervisor because: #{inspect reason}")
+		end
+	end
 end
